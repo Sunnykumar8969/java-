6bab5de98850d8b3b4e261563c53fff18d1ef4dd
Sunnykumar8969 <98380675+Sunnykumar8969@users.noreply.github.com>
2022-02-03 23:31:49 +0530

    Add files via upload

---
+++ world.java
@@ -0,0 +1,11 @@
+public class world {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Sunny kumar");
+
+System.out.println("hello");
+System.out.print("Welcome");
+	}
+
+}
